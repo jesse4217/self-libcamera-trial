582cce3534e2b4b70a05f7f255f6506c07b83044
jesse4217 <jiang@berryinc.co.jp>
2025-08-27 13:36:03 +0900

    added capture

---
+++ inc/multicam.h
@@ -0,0 +1,14 @@
+#ifndef MULTICAM_H
+#define MULTICAM_H
+
+#include <cstdio>
+#include <iomanip>
+#include <memory>
+#include <thread>
+
+#include <libcamera/libcamera.h>
+
+using namespace libcamera;
+using namespace std::chrono_literals;
+
+#endif // MULTICAM_H
